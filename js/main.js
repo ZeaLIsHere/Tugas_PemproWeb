fbdc6e7466cb6b71daef63792627f229a192d212
ZeaL <tariq.rahmadari23@gmail.com>
2025-10-01 13:29:32 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,11 @@
-// Toggle & Responsive Navigation
+const burger = document.querySelector(".burger");
+const navList = document.querySelector("nav ul");
+
 const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const navLists = document.querySelector("nav");
-  
-    burger.addEventListener("click", () => {
-      // Toggle nav list and burger class
-      navLists.classList.toggle("nav-active");
-      burger.classList.toggle("toggle-burger");
-    });
-  };
-  
-  navSlide();
-  
-  // Clear form before unload
-  window.onbeforeunload = () => {
-    for (const form of document.getElementsByTagName("form")) {
-      form.reset();
-    }
-  };
+  burger.addEventListener("click", () => {
+    navList.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
